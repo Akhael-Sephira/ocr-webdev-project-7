8e928cce23edaba890a3b0f9357d7682bb9efdc0
Akhael <akhaeldelacroix@gmail.com>
2022-12-04 16:23:35 +0100

    Making NotFound page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+import './NotFound.css'
+
 export default function NotFound() {
     return (
-        <div>NotFound</div>
+        <>
+            <p className="notfound-title">404</p>
+            <p className="notfound-text">Oups! La page que vous demandez n'existe pas.</p>
+            <Link className="notfound-link" to="/">Retourner sur la page d'accueil</Link>
+        </>
     )
 }
